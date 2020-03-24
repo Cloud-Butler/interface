6aea1a34b14ec3c4a854375e9d4e85852b0f745e
Charles Christensen <30779702+charlesrc19@users.noreply.github.com>
2020-03-24 21:03:40 +0000

    Testing ENV passing into bundle.

--- bin/add-owner.sh
+++ bin/add-owner.sh
@@ -3,14 +3,9 @@
 # Verify values.
 if [ ${COG_OPT_USER} ] || [ ${COG_OPT_PASSWORD} ]; then
     echo "Password or user-to-adjust not specified."
-    exit 1
-fi
-if [[ ${COG_OPT_PASSWORD} != ${CB_ADMIN_PASSWORD} ]]; then
+elif [[ ${COG_OPT_PASSWORD} != ${CB_ADMIN_PASSWORD} ]]; then
     echo "Password is incorrect."
-    exit 1
+else
+    cogctl group add cog-admin ${COG_OPT_USER}
+    echo "The user '${COG_OPT_USER}' has been added as a chatbot owner."
 fi
-
-# Perform priviledge adjustment.
-cogctl group add cog-admin ${COG_OPT_USER}
-echo "The user '${COG_OPT_USER}' has been added as a chatbot owner."
-exit 0
