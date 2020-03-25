d7b7b8e61a5811723ba224add896cc831b9f0a0b
Charles Christensen <30779702+charlesrc19@users.noreply.github.com>
2020-03-24 18:57:51 -0600

    Update add-owner.sh

--- bin/add-owner.sh
+++ bin/add-owner.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cogctl profile create default http://localhost:4000 "admin" "${COG_OPT_PASSWORD}"
+cogctl profile create default http://$(ip route show | awk '/default/ {print $3}'):80 "admin" "${COG_OPT_PASSWORD}"
 cogctl group add cog-admin ${COG_OPT_USER}
